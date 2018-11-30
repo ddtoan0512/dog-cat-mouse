768cc6692ae652fb1e06011e59f1bb54d2510300
ToanDang <ddtoan991205@gmail.com>
2018-11-30 16:45:09 +0700

    Comment all code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var Dog = require('./Dog');
-var Cat = require('./Cat');
+// var Dog = require('./Dog');
+// var Cat = require('./Cat');
 
-var dog = new Dog('Tom');
-var tom = new Cat();
+// var dog = new Dog('Tom');
+// var tom = new Cat();
 
-dog.eat(tom);
+// dog.eat(tom);
+// console.log(dog)
 
